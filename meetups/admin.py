84b225b0d24340ef379c3a97eabca43f4145e2aa
salegrettm <salegrett-morreo@huskers.unl.edu>
2023-02-05 17:01:10 -0600

    Added Location Model and addresses for meetups 📍

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Meetup
+from .models import Meetup, Location
 
 class MeetupAdmin(admin.ModelAdmin):
   list_display = ('title', 'slug')
-  list_filter = ('title',)
+  list_filter = ('title', 'location')
   prepopulated_fields = { 'slug': ('title', )}
 
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
