a08f5ffabfcc37f163b7ffca38b7827505dc31a1
salegrettm <salegrett-morreo@huskers.unl.edu>
2023-02-05 17:41:35 -0600

    Added post method for registration forms ✍🏽

--- meetups/views.py
+++ meetups/views.py
@@ -14,12 +14,19 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
-        registration_form = RegistrationForm()
-        return render(request, 'meetups/meetup-details.html', {
-            'meetup_found': True,
-            'meetup': selected_meetup,
-            'form': registration_form
-        })
+        if request.method == "GET":
+            registration_form = RegistrationForm()
+            return render(request, 'meetups/meetup-details.html', {
+                'meetup_found': True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)
+
     except Exception as exc:
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': False
