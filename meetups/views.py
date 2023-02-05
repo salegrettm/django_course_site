d5af210c91aca407a04e23f53404ed05551e13ee
salegrettm <salegrett-morreo@huskers.unl.edu>
2023-02-04 18:13:39 -0600

    Added all css and rendered correct index 💇🏽‍♂️

--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hello World')
+    return render(request, 'meetups/index.html')
