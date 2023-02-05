6b6b5d48ab8b6648cf9a98159c1848beb5504c4c
salegrettm <salegrett-morreo@huskers.unl.edu>
2023-02-05 14:42:21 -0600

    Added images to Meetup model and served 😁: - Configured admin area to display model info.

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Meetup
-# Register your models here.
-admin.site.register(Meetup)
+
+class MeetupAdmin(admin.ModelAdmin):
+  list_display = ('title', 'slug')
+  list_filter = ('title',)
+  prepopulated_fields = { 'slug': ('title', )}
+
+admin.site.register(Meetup, MeetupAdmin)
--- meetups/models.py
+++ meetups/models.py
@@ -4,3 +4,7 @@ class Meetup(models.Model):
   title = models.CharField(max_length=200)
   slug = models.SlugField(unique=True)
   description = models.TextField()
+  image = models.ImageField(upload_to='images')
+
+  def __str__(self):
+    return f'{self.title} - {self.slug}'
