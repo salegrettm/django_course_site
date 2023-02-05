6ed602d1e2f2484babe7f8143ef202fcf0d4835c
salegrettm <salegrett-morreo@huskers.unl.edu>
2023-02-05 17:21:51 -0600

    Added ✅: - Participants Model - M2M between Participants and Meetups - Image to meetup details

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Meetup, Location
+from .models import Meetup, Location, Participant
 
 class MeetupAdmin(admin.ModelAdmin):
-  list_display = ('title', 'slug')
-  list_filter = ('title', 'location')
+  list_display = ('title', 'date', 'location')
+  list_filter = ('title', 'location', 'date')
   prepopulated_fields = { 'slug': ('title', )}
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
--- meetups/models.py
+++ meetups/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 
+
 class Location(models.Model):
-  name = models.CharField(max_length=200)
-  address = models.CharField(max_length=300)
-  
-  def __str__(self):
-    return f'{self.name} ({self.address})'
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
+
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
 
 class Meetup(models.Model):
-  title = models.CharField(max_length=200)
-  slug = models.SlugField(unique=True)
-  description = models.TextField()
-  image = models.ImageField(upload_to='images')
-  location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    organizer_email = models.EmailField()
+    date = models.DateField()
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+    location = models.ForeignKey(
+        Location, on_delete=models.SET_NULL, null=True)
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
 
-  def __str__(self):
-    return f'{self.title} - {self.slug}'
+    def __str__(self):
+        return f'{self.title} - {self.slug}'
