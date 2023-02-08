ef3d601c0e646e69c7b80eacee8ab5a056b3c3b7
salegrettm <salegrett-morreo@huskers.unl.edu>
2023-02-07 20:15:43 -0600

    Added success confirmation for registration 👍

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'),  # our-domain.com/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail') # our-domain.com/meetups/<meetup_slug>
+    path('meetups/', views.index, name='all-meetups'),
+    path('meetups/success', views.confirm_registration,
+         name='confirm-registration'),
+    path('meetups/<slug:meetup_slug>', views.meetup_details,
+         name='meetup-detail')
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Meetup
 from .forms import RegistrationForm
 
@@ -26,8 +26,17 @@ def meetup_details(request, meetup_slug):
             if registration_form.is_valid():
                 participant = registration_form.save()
                 selected_meetup.participants.add(participant)
-
+                return redirect('confirm-registration')
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup': selected_meetup,
+            'form': registration_form
+        })
     except Exception as exc:
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': False
         })
+
+
+def confirm_registration(request):
+    return render(request, 'meetups/registration-success.html')
